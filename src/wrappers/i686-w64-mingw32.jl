e73be410ea0410a59150a72e4837dc231dfff8ee
jlbuild <juliabuildbot@gmail.com>
2020-10-16 19:01:02 +0000

    Fontconfig_jll build 2.13.1+14

--- src/wrappers/i686-w64-mingw32.jl
+++ src/wrappers/i686-w64-mingw32.jl
@@ -6,374 +6,78 @@ using Bzip2_jll
 using Zlib_jll
 using Libuuid_jll
 using Expat_jll
-## Global variables
-PATH = ""
-LIBPATH = ""
-LIBPATH_env = "PATH"
-LIBPATH_default = ""
-
-# Relative path to `fc_cache`
-const fc_cache_splitpath = ["bin", "fc-cache.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_cache_path = ""
-
-# fc_cache-specific global declaration
-function fc_cache(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_cache_path)
-    end
-end
-
-
-# Relative path to `fc_cat`
-const fc_cat_splitpath = ["bin", "fc-cat.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_cat_path = ""
-
-# fc_cat-specific global declaration
-function fc_cat(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_cat_path)
-    end
-end
-
-
-# Relative path to `fc_conflist`
-const fc_conflist_splitpath = ["bin", "fc-conflist.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_conflist_path = ""
-
-# fc_conflist-specific global declaration
-function fc_conflist(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_conflist_path)
-    end
-end
-
-
-# Relative path to `fc_list`
-const fc_list_splitpath = ["bin", "fc-list.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_list_path = ""
-
-# fc_list-specific global declaration
-function fc_list(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_list_path)
-    end
-end
-
-
-# Relative path to `fc_match`
-const fc_match_splitpath = ["bin", "fc-match.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_match_path = ""
-
-# fc_match-specific global declaration
-function fc_match(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_match_path)
-    end
-end
-
-
-# Relative path to `fc_pattern`
-const fc_pattern_splitpath = ["bin", "fc-pattern.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_pattern_path = ""
-
-# fc_pattern-specific global declaration
-function fc_pattern(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_pattern_path)
-    end
-end
-
-
-# Relative path to `fc_query`
-const fc_query_splitpath = ["bin", "fc-query.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_query_path = ""
-
-# fc_query-specific global declaration
-function fc_query(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_query_path)
-    end
-end
-
-
-# Relative path to `fc_scan`
-const fc_scan_splitpath = ["bin", "fc-scan.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_scan_path = ""
-
-# fc_scan-specific global declaration
-function fc_scan(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_scan_path)
-    end
-end
-
-
-# Relative path to `fc_validate`
-const fc_validate_splitpath = ["bin", "fc-validate.exe"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fc_validate_path = ""
-
-# fc_validate-specific global declaration
-function fc_validate(f::Function; adjust_PATH::Bool = true, adjust_LIBPATH::Bool = true)
-    global PATH, LIBPATH
-    env_mapping = Dict{String,String}()
-    if adjust_PATH
-        if !isempty(get(ENV, "PATH", ""))
-            env_mapping["PATH"] = string(PATH, ';', ENV["PATH"])
-        else
-            env_mapping["PATH"] = PATH
-        end
-    end
-    if adjust_LIBPATH
-        LIBPATH_base = get(ENV, LIBPATH_env, expanduser(LIBPATH_default))
-        if !isempty(LIBPATH_base)
-            env_mapping[LIBPATH_env] = string(LIBPATH, ';', LIBPATH_base)
-        else
-            env_mapping[LIBPATH_env] = LIBPATH
-        end
-    end
-    withenv(env_mapping...) do
-        f(fc_validate_path)
-    end
-end
-
-
-# Relative path to `fonts_conf`
-const fonts_conf_splitpath = ["etc", "fonts", "fonts.conf"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-fonts_conf_path = ""
-
-# fonts_conf-specific global declaration
-# This will be filled out by __init__()
-fonts_conf = ""
-
-
-# Relative path to `libfontconfig`
-const libfontconfig_splitpath = ["bin", "libfontconfig-1.dll"]
-
-# This will be filled out by __init__() for all products, as it must be done at runtime
-libfontconfig_path = ""
-
-# libfontconfig-specific global declaration
-# This will be filled out by __init__()
-libfontconfig_handle = C_NULL
-
-# This must be `const` so that we can use it with `ccall()`
-const libfontconfig = "libfontconfig-1.dll"
-
-
-"""
-Open all libraries
-"""
+JLLWrappers.@generate_wrapper_header("Fontconfig")
+JLLWrappers.@declare_executable_product(fc_cache)
+JLLWrappers.@declare_executable_product(fc_cat)
+JLLWrappers.@declare_executable_product(fc_conflist)
+JLLWrappers.@declare_executable_product(fc_list)
+JLLWrappers.@declare_executable_product(fc_match)
+JLLWrappers.@declare_executable_product(fc_pattern)
+JLLWrappers.@declare_executable_product(fc_query)
+JLLWrappers.@declare_executable_product(fc_scan)
+JLLWrappers.@declare_executable_product(fc_validate)
+JLLWrappers.@declare_file_product(fonts_conf)
+JLLWrappers.@declare_library_product(libfontconfig, "libfontconfig-1.dll")
 function __init__()
-    global artifact_dir = abspath(artifact"Fontconfig")
+    JLLWrappers.@generate_init_header(FreeType2_jll, Bzip2_jll, Zlib_jll, Libuuid_jll, Expat_jll)
+    JLLWrappers.@init_executable_product(
+        fc_cache,
+        "bin\\fc-cache.exe",
+    )
 
-    # Initialize PATH and LIBPATH environment variable listings
-    global PATH_list, LIBPATH_list
-    # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
-    # then append them to our own.
-    foreach(p -> append!(PATH_list, p), (FreeType2_jll.PATH_list, Bzip2_jll.PATH_list, Zlib_jll.PATH_list, Libuuid_jll.PATH_list, Expat_jll.PATH_list,))
-    foreach(p -> append!(LIBPATH_list, p), (FreeType2_jll.LIBPATH_list, Bzip2_jll.LIBPATH_list, Zlib_jll.LIBPATH_list, Libuuid_jll.LIBPATH_list, Expat_jll.LIBPATH_list,))
+    JLLWrappers.@init_executable_product(
+        fc_cat,
+        "bin\\fc-cat.exe",
+    )
 
-    global fc_cache_path = normpath(joinpath(artifact_dir, fc_cache_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_conflist,
+        "bin\\fc-conflist.exe",
+    )
 
-    push!(PATH_list, dirname(fc_cache_path))
-    global fc_cat_path = normpath(joinpath(artifact_dir, fc_cat_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_list,
+        "bin\\fc-list.exe",
+    )
 
-    push!(PATH_list, dirname(fc_cat_path))
-    global fc_conflist_path = normpath(joinpath(artifact_dir, fc_conflist_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_match,
+        "bin\\fc-match.exe",
+    )
 
-    push!(PATH_list, dirname(fc_conflist_path))
-    global fc_list_path = normpath(joinpath(artifact_dir, fc_list_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_pattern,
+        "bin\\fc-pattern.exe",
+    )
 
-    push!(PATH_list, dirname(fc_list_path))
-    global fc_match_path = normpath(joinpath(artifact_dir, fc_match_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_query,
+        "bin\\fc-query.exe",
+    )
 
-    push!(PATH_list, dirname(fc_match_path))
-    global fc_pattern_path = normpath(joinpath(artifact_dir, fc_pattern_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_scan,
+        "bin\\fc-scan.exe",
+    )
 
-    push!(PATH_list, dirname(fc_pattern_path))
-    global fc_query_path = normpath(joinpath(artifact_dir, fc_query_splitpath...))
+    JLLWrappers.@init_executable_product(
+        fc_validate,
+        "bin\\fc-validate.exe",
+    )
 
-    push!(PATH_list, dirname(fc_query_path))
-    global fc_scan_path = normpath(joinpath(artifact_dir, fc_scan_splitpath...))
+    JLLWrappers.@init_file_product(
+        fonts_conf,
+        "etc\\fonts\\fonts.conf",
+    )
 
-    push!(PATH_list, dirname(fc_scan_path))
-    global fc_validate_path = normpath(joinpath(artifact_dir, fc_validate_splitpath...))
+    JLLWrappers.@init_library_product(
+        libfontconfig,
+        "bin\\libfontconfig-1.dll",
+        RTLD_LAZY | RTLD_DEEPBIND,
+    )
 
-    push!(PATH_list, dirname(fc_validate_path))
-    global fonts_conf_path = normpath(joinpath(artifact_dir, fonts_conf_splitpath...))
-
-    global fonts_conf = fonts_conf_path
-    global libfontconfig_path = normpath(joinpath(artifact_dir, libfontconfig_splitpath...))
-
-    # Manually `dlopen()` this right now so that future invocations
-    # of `ccall` with its `SONAME` will find this path immediately.
-    global libfontconfig_handle = dlopen(libfontconfig_path)
-    push!(LIBPATH_list, dirname(libfontconfig_path))
-
-    # Filter out duplicate and empty entries in our PATH and LIBPATH entries
-    filter!(!isempty, unique!(PATH_list))
-    filter!(!isempty, unique!(LIBPATH_list))
-    global PATH = join(PATH_list, ';')
-    global LIBPATH = join(vcat(LIBPATH_list, [Sys.BINDIR, joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ';')
-
+    JLLWrappers.@generate_init_footer()
     ENV["FONTCONFIG_FILE"] = get(ENV, "FONTCONFIG_FILE", fonts_conf)
     ENV["FONTCONFIG_PATH"] = get(ENV, "FONTCONFIG_PATH", dirname(ENV["FONTCONFIG_FILE"]))
 
 end  # __init__()
-
